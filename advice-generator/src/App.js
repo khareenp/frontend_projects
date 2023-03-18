c7bae5f015933715ba57b5b27caf55151232efd3
khareenp <khareenp1@gmail.com>
2023-03-18 14:43:53 -0400

    generator styling

--- advice-generator/src/App.js
+++ advice-generator/src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import dice from "./icon-dice.svg";
+import divider from "./pattern-divider-desktop.svg";
 
 export default function App() {
   const [advice, setAdvice] = useState({});
@@ -20,15 +22,21 @@ export default function App() {
   }, []);
 
   return (
-    <div className="display flex flex-col">
-      <div className="card text-lg"> Advice {advice.id}</div>
-      <p>{advice.advice}</p>
-      <button onClick={getAdvice}>
-        <img
-          src="../advice-generator-app-main/images/icon-dice.svg"
-          alt=""
-        ></img>
-      </button>
+    <div className="display flex w-full h-screen justify-center items-center bg-slate-900 text-gray-400">
+      <div className="card flex flex-col bg-gray-700 w-2/6 h-1/3 p-9 text-center items-center rounded-lg justify-between font-bold">
+        <div className="title text-green-500 tracking-widest text-sm">
+          {" "}
+          Advice #{advice.id}
+        </div>
+        <p className=" text-2xl">"{advice.advice}"</p>
+        <img src={divider} alt="divider" className="mb-6"></img>
+        <button
+          className="border-0 border-solid rounded-full absolute bg-green-500 p-5 mt-60"
+          onClick={getAdvice}
+        >
+          <img src={dice} alt="x" className=" h-8 w-8"></img>
+        </button>
+      </div>
     </div>
   );
 }
