e4045854ce54b6d2bd35fe6950cd001061081155
khareenp <khareenp1@gmail.com>
2023-04-08 23:38:52 -0400

    completed

--- movie-land/src/App.js
+++ movie-land/src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import searchIcon from "./search.svg";
 
+import "./App.css";
+import MovieCard from "./MovieCard";
+
+// keycode: ef81a621
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  console.log(movies);
+
+  const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=ef81a621";
+
+  // const searchMovies = async (title) => {
+  //   const response = await fetch(`${API_URL}&s=${title}`);
+  //   const data = await response.data;
+  //   console.log(response);
+  // };
+
+  async function searchMovies(title) {
+    try {
+      const response = await axios.get(`${API_URL}&s=${title}`);
+      const data = response.data.Search;
+      setMovies(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    searchMovies("Spiderman");
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>MovieLand</h1>
+      <div className="search">
+        <input
+          placeholder="Search for movies"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        ></input>
+        <img
+          src={searchIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
+      {movies.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No Movies Found</h2>
+        </div>
+      )}
     </div>
   );
 }
---
+++ movie-land/src/MovieCard.jsx
@@ -0,0 +1,26 @@
+const MovieCard = ({ movie }) => {
+  return (
+    <div>
+      <div className="movie">
+        <div>
+          <p>{movie.Year}</p>
+        </div>
+        <div>
+          <img
+            src={
+              movie.Poster !== "N/A"
+                ? movie.Poster
+                : "https://via.placeholder.com/400"
+            }
+            alt="poster"
+          />
+        </div>
+        <div>
+          <span>{movie.Type}</span>
+          <h3>{movie.Title}</h3>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default MovieCard;
