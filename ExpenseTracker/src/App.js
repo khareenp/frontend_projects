8666918fa95c49b4ab228df7dd1b934a029558a6
khareenp <khareenp1@gmail.com>
2023-04-09 16:07:23 -0400

    update components

--- ExpenseTracker/src/App.js
+++ ExpenseTracker/src/App.js
@@ -1,23 +1,23 @@
-import { ExpenseItem } from "./components/ExpenseItem";
+import Expenses from './components/Expenses';
 
 function App() {
   const expenses = [
     {
-      id: "e1",
-      title: "Toilet Paper",
+      id: 'e1',
+      title: 'Toilet Paper',
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
     {
-      id: "e3",
-      title: "Car Insurance",
+      id: 'e3',
+      title: 'Car Insurance',
       amount: 294.67,
       date: new Date(2021, 2, 28),
     },
     {
-      id: "e4",
-      title: "New Desk (Wooden)",
+      id: 'e4',
+      title: 'New Desk (Wooden)',
       amount: 450,
       date: new Date(2021, 5, 12),
     },
@@ -25,9 +25,8 @@ function App() {
 
   return (
     <div>
-      {expenses.map(({ title, id, amount, date }) => (
-        <ExpenseItem title={title} id={id} amount={amount} date={date} />
-      ))}
+      <h2>Let's get started!</h2>
+      <Expenses items={expenses} />
     </div>
   );
 }
