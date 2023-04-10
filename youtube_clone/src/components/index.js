7f923bc684d932396599066394b26df23e891293
khareenp <khareenp1@gmail.com>
2023-04-10 14:43:17 -0400

    removed

--- youtube_clone/src/components/index.js
+++
@@ -1,6 +0,0 @@
-//for multiple imports to prevent clutering App.js
-export { default as Navbar } from "./Navbar";
-export { default as Feed } from "./Feed";
-export { default as SearchFeed } from "./SearchFeed";
-export { default as VideoDetail } from "./VideoDetail";
-export { default as ChannelDetail } from "./ChannelDetail";
